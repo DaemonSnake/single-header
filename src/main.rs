d4ba51ac5b829b30e04b83b1471864caae77eaeb
Bastien Penavayre <bastienPenava@gmail.com>
2024-01-23 20:46:56 +0100

    use enums for clap options and refactor double include protection

--- src/main.rs
+++ src/main.rs
@@ -24,18 +24,59 @@ struct Ops {
     #[arg(help = "path to c/c++ header file")]
     file: String,
 
-    #[arg(default_value = "c++", short = 'x', long = "lang")]
-    lang: String,
+    #[arg(default_value = Lang::Cpp.as_str(), short = 'x', long = "lang")]
+    #[clap(value_enum)]
+    lang: Lang,
 
     #[arg(
         long = "protect",
         default_value = "ifdef",
         help = "protect against multiple includes with `#ifdef` or `#pragma once`"
     )]
-    protection: String,
+    #[clap(value_enum)]
+    protection: Protection,
 
     #[arg(help = r"additional parameters for `cpp`")]
     cpp_opts: Option<Vec<String>>,
+}
+
+#[derive(clap::ValueEnum, Clone, Debug)]
+enum Lang {
+    C,
+    #[clap(name = "c++")]
+    Cpp,
+}
+
+impl Lang {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Lang::C => "c",
+            Lang::Cpp => "c++",
+        }
+    }
+}
+
+#[derive(clap::ValueEnum, Clone, Debug)]
+enum Protection {
+    Ifdef,
+    Once,
+}
+
+fn pragme_once(action: impl FnOnce(), _filename: String) {
+    println!("#pragma once");
+    action();
+}
+
+fn ifndef_guard(action: impl FnOnce(), filename: String) {
+    let invalid_macro_char = |c: char| !char::is_alphanumeric(c) && c != '_';
+
+    let macro_name = filename.to_uppercase().replace(invalid_macro_char, "_");
+    let macro_name = format!("{macro_name}_SINGLE_HEADER"); // prevent collisions with user-land include guards
+
+    println!("#ifndef {macro_name}");
+    println!("# define {macro_name}");
+    action();
+    println!("#endif // {macro_name}");
 }
 
 fn main() {
@@ -46,7 +87,7 @@ fn main() {
 
     let output = Command::new("cpp")
         .arg("-x")
-        .arg(ops.lang)
+        .arg(ops.lang.as_str())
         .arg(&ops.file)
         .args(default_cpp_opts)
         .args(extra_cpp_opts)
@@ -56,24 +97,15 @@ fn main() {
     let txt = std::str::from_utf8(output.stdout.as_slice()).expect("cpp output isn't utf-8");
     let lines = txt.lines();
 
-    let invalid_macro_char = |c: char| !char::is_alphanumeric(c) && c != '_';
+    let protection = match ops.protection {
+        Protection::Ifdef => ifndef_guard,
+        Protection::Once => pragme_once,
+    };
 
-    let def_name = ops.file.to_uppercase().replace(invalid_macro_char, "_");
-    let def_name = format!("{def_name}_SINGLE_HEADER"); // prevent collisions with user-land include guards
-    let is_pragma = matches!(ops.protection.as_str(), "once");
-
-    {
-        if is_pragma {
-            println!("#pragma once");
-        } else {
-            println!("#ifndef {def_name}");
-            println!("# define {def_name}");
-        }
-    }
-    process_lines(lines);
-    {
-        if !is_pragma {
-            println!("#endif // {def_name}")
-        }
-    }
+    protection(
+        || {
+            process_lines(lines);
+        },
+        ops.file,
+    );
 }
