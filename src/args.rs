ea5a2eab9f178f3c5d36b9a17a48a1eaa29334e2
Bastien Penavayre <bastienPenava@gmail.com>
2024-01-24 17:15:19 +0100

    src/args.rs move args into module and cleanup

---
+++ src/args.rs
@@ -0,0 +1,71 @@
+#[derive(clap::ValueEnum, Clone, Debug)]
+pub enum Preprocessor {
+    Cpp,
+    Gcc,
+    Clang,
+}
+
+impl Preprocessor {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Preprocessor::Cpp => "cpp",
+            Preprocessor::Gcc => "gcc",
+            Preprocessor::Clang => "clang",
+        }
+    }
+
+    pub fn required_args(&self) -> Vec<&'static str> {
+        match self {
+            Preprocessor::Cpp => vec![],
+            Preprocessor::Gcc | Preprocessor::Clang => vec!["-E"],
+        }
+    }
+}
+
+#[derive(clap::ValueEnum, Clone, Debug)]
+pub enum Lang {
+    C,
+    #[clap(name = "c++")]
+    Cpp,
+}
+
+impl Lang {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Lang::C => "c",
+            Lang::Cpp => "c++",
+        }
+    }
+}
+
+#[derive(clap::ValueEnum, Clone, Debug)]
+pub enum Protection {
+    Ifdef,
+    Once,
+}
+
+impl Protection {
+    pub fn protect(&self, action: impl FnOnce(), filename: String) {
+        match self {
+            Protection::Ifdef => ifndef_guard(action, filename),
+            Protection::Once => pragme_once(action),
+        }
+    }
+}
+
+fn pragme_once(action: impl FnOnce()) {
+    println!("#pragma once");
+    action();
+}
+
+fn ifndef_guard(action: impl FnOnce(), filename: String) {
+    let invalid_macro_char = |c: char| !char::is_alphanumeric(c) && c != '_';
+
+    let macro_name = filename.to_uppercase().replace(invalid_macro_char, "_");
+    let macro_name = format!("{macro_name}_SINGLE_HEADER"); // prevent collisions with user-land include guards
+
+    println!("#ifndef {macro_name}");
+    println!("# define {macro_name}");
+    action();
+    println!("#endif // {macro_name}");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+mod args;
 mod include_line;
 mod line_zero;
 mod process;
 mod system_paths;
 
+use args::{Lang, Preprocessor, Protection};
 use clap::Parser;
 use process::process_lines;
 use std::process::Command;
@@ -27,81 +29,24 @@ struct Ops {
     #[arg(
         default_value = Preprocessor::Cpp.as_str(),
         short = 'p',
-        long = "preprocessor"
+        long = "preprocessor",
+        value_enum
     )]
-    #[clap(value_enum)]
     preprocessor: Preprocessor,
 
-    #[arg(default_value = Lang::Cpp.as_str(), short = 'x', long = "lang")]
-    #[clap(value_enum)]
+    #[arg(default_value = Lang::Cpp.as_str(), short = 'x', long = "lang", value_enum)]
     lang: Lang,
 
     #[arg(
         long = "protect",
         default_value = "ifdef",
-        help = "protect against multiple includes with `#ifdef` or `#pragma once`"
+        help = "protect against multiple includes with `#ifdef` or `#pragma once`",
+        value_enum
     )]
-    #[clap(value_enum)]
     protection: Protection,
 
     #[arg(help = r"additional parameters for the preprocessor")]
     cpp_opts: Option<Vec<String>>,
-}
-
-#[derive(clap::ValueEnum, Clone, Debug)]
-enum Preprocessor {
-    Cpp,
-    Gcc,
-    Clang,
-}
-
-impl Preprocessor {
-    fn as_str(&self) -> &'static str {
-        match self {
-            Preprocessor::Cpp => "cpp",
-            Preprocessor::Gcc => "gcc",
-            Preprocessor::Clang => "clang",
-        }
-    }
-}
-
-#[derive(clap::ValueEnum, Clone, Debug)]
-enum Lang {
-    C,
-    #[clap(name = "c++")]
-    Cpp,
-}
-
-impl Lang {
-    fn as_str(&self) -> &'static str {
-        match self {
-            Lang::C => "c",
-            Lang::Cpp => "c++",
-        }
-    }
-}
-
-#[derive(clap::ValueEnum, Clone, Debug)]
-enum Protection {
-    Ifdef,
-    Once,
-}
-
-fn pragme_once(action: impl FnOnce(), _filename: String) {
-    println!("#pragma once");
-    action();
-}
-
-fn ifndef_guard(action: impl FnOnce(), filename: String) {
-    let invalid_macro_char = |c: char| !char::is_alphanumeric(c) && c != '_';
-
-    let macro_name = filename.to_uppercase().replace(invalid_macro_char, "_");
-    let macro_name = format!("{macro_name}_SINGLE_HEADER"); // prevent collisions with user-land include guards
-
-    println!("#ifndef {macro_name}");
-    println!("# define {macro_name}");
-    action();
-    println!("#endif // {macro_name}");
 }
 
 fn main() {
@@ -116,10 +61,7 @@ fn main() {
     };
 
     let base_preprocessor_args = {
-        let preprocessor_only = match ops.preprocessor {
-            Preprocessor::Gcc | Preprocessor::Clang => vec!["-E"],
-            Preprocessor::Cpp => vec![],
-        };
+        let preprocessor_only = ops.preprocessor.required_args();
 
         let base_args = vec![
             "-x",
@@ -133,10 +75,10 @@ fn main() {
             .collect::<Vec<_>>()
     };
 
-    let extra_cpp_opts = ops.cpp_opts.clone().unwrap_or_else(|| Vec::new());
-
     let search_paths =
         system_paths::SearchPaths::new(ops.preprocessor.as_str(), &base_preprocessor_args);
+
+    let extra_cpp_opts = ops.cpp_opts.unwrap_or_else(|| Vec::new());
 
     let output = Command::new(ops.preprocessor.as_str())
         .args(&base_preprocessor_args)
@@ -156,12 +98,7 @@ fn main() {
     let txt = std::str::from_utf8(output.stdout.as_slice()).expect("cpp output isn't utf-8");
     let lines = txt.lines();
 
-    let protection = match ops.protection {
-        Protection::Ifdef => ifndef_guard,
-        Protection::Once => pragme_once,
-    };
-
-    protection(
+    ops.protection.protect(
         || {
             process_lines(lines, search_paths);
         },
